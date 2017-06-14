c884b8b370f798c1c229af5f243f5529ee511846
Miguel Pessoa <guelmisp@gmail.com>
2017-06-14 15:20:09 -0300

    update trabalho1

--- trabalho01/src/tra1/TCPClient.java
+++ trabalho01/src/tra1/TCPClient.java
@@ -4,37 +4,79 @@ import java.net.*;
 import java.util.Scanner;
 import java.io.*;
 public class TCPClient {
-	@SuppressWarnings("deprecation")
 	public static void main (String args[]) {
 		// arguments supply message and host name
 		Socket s = null;
 		try{
 			
 			/*initial setup of the server*/
-			int port = 7896;
+			int port = 7897;
 			String address = "127.0.0.1";
 			String comando = null;
 			String data = null;
+			
+			/*Setup to transfer files*/
+			FileInputStream fis = null;
+			BufferedInputStream bis = null;
+			OutputStream os = null;
 			
 			/*creating socket and channels of input and output*/
 			s = new Socket(address, port);    
 			DataInputStream in = new DataInputStream( s.getInputStream());
 			
-			DataOutputStream out =new DataOutputStream( s.getOutputStream());
+			DataOutputStream out = new DataOutputStream( s.getOutputStream());
 			Scanner scanner = new Scanner(System.in);
 
 			while(true){
 				comando = scanner.nextLine();
-				out.writeUTF(comando);
-				
-				data = in.readUTF();
-				System.out.println(data);
-				
 				/*close client*/
 				if (comando.contains("exit")){
 					System.out.println("Encerrando cliente...");
 					s.close();
+				} else if (comando.contains("sendfile")){
+					try {
+						/*tell the server to receive a file*/
+						out.writeUTF(comando);
+						data = in.readUTF();
+						System.out.println(data);
+						if (data.contains("ready!")){
+							String FILE_PATH = comando.replaceAll("sendfile ", "");
+							
+							/*creating a new file to send*/
+							File clientFile = new File (FILE_PATH);
+							byte [] clientArray = new byte[(int) clientFile.length()];
+							fis = new FileInputStream(clientFile);
+							bis = new BufferedInputStream(fis);
+							bis.read(clientArray,0,clientArray.length);
+							
+							/*Send file size*/
+							out.writeLong(clientArray.length);
+							
+							/*Send file*/
+							out.write(clientArray, 0, clientArray.length);
+							out.flush();
+							
+							/*verify*/
+							System.out.println(in.readUTF());
+							
+						} else {
+							System.out.println("Servidor nao esta pronto para receber arquivo.");
+						}
+						
+					} catch (IOException ex){
+						System.out.println(ex.getMessage());
+					} finally {
+						if (bis != null) bis.close();
+						if (os != null) os.close();
+						if (fis != null) fis.close();
+					}
+					
+				} else {
+					out.writeUTF(comando);
+					data = in.readUTF();
+					System.out.println(data);
 				}
+				
 			}
 		}
 		catch (UnknownHostException e){
--- trabalho01/src/tra1/TCPServer.java
+++ trabalho01/src/tra1/TCPServer.java
@@ -6,7 +6,7 @@ import java.io.*;
 public class TCPServer {
 	public static void main (String args[]) {
 		try{
-			int serverPort = 7896; // the server port
+			int serverPort = 7897; // the server port
 			ServerSocket listenSocket = new ServerSocket(serverPort);
 			while(true) {
 				Socket clientSocket = listenSocket.accept();
@@ -42,8 +42,15 @@ class Connection extends Thread {
 		String clientPath = mainPath + clientSocket.getPort() + "/";
 		new File(clientPath).mkdirs();
 		
+		/*setup to receive file*/
+		BufferedOutputStream bos = null;
+		OutputStream fos = null;
+		int bytesRead;
+		
 		try {
 			while(true){
+				
+				System.out.println("Servidor pronto...");
 				String data = in.readUTF();	  
 				
 				if(data.contains("mkdir")){
@@ -73,11 +80,38 @@ class Connection extends Thread {
 				    }
 				    
 				    /*Send answer to client*/
-					out.writeUTF(resposta.toString() + "/n");
+					out.writeUTF(resposta.toString() + "\n");
 			        out.flush();
 				}
 				else if (data.contains("sendfile")){
-					out.writeUTF("Enviar arquivo para o servidor");
+					try {
+						System.out.println("Cliente: " + clientSocket + " | Tarefa: sendfile");
+						/*get file name*/
+						String filePath =  data.replaceAll("sendfile", "");
+						String[] parts = filePath.split("/");
+						String fileName = clientPath + parts[parts.length-1];
+						
+						/*tell client to send the file*/
+						out.writeUTF("ready!");
+						
+						/*gets file size from Client*/
+						long fileSize = in.readLong();
+							
+						/*recreate the file and save it in Client directory*/
+						byte [] buffer = new byte [1024];
+						fos = new FileOutputStream(fileName);
+	
+						while (fileSize > 0 && (bytesRead = in.read(buffer, 0, (int)Math.min(buffer.length, fileSize))) != -1) {
+							fos.write(buffer, 0, bytesRead);
+							fileSize -= bytesRead;
+						}
+						
+						out.writeUTF("Arquivo salvo no servidor.");
+						
+					} finally {
+						if (bos != null) bos.close();
+						if (fos != null) fos.close();
+					}
 				}
 				else if (data.contains("exit")){
 					out.writeUTF("terminando execucao com servidor...");
